dff1e685ba5e76b90bc8f8c875ca39a36762a4fe
Jeff Fowler <jeffowler@gmail.com>
2016-09-17 01:10:40 -0400

    dumb function that changes the color var

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ ReactDOM.render(
     <h1 style={ {"color":color} }>{thing}</h1>,
     document.getElementById('example')
 );
+
+setInterval(function() {
+    color = colors[Math.floor(Math.random() * colors.length)];
+    console.log(color);
+}, 1000)
