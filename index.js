8afbc12f5919d05ca1eaee1e0a50790e6ca0da41
Jeff Fowler <jeffowler@gmail.com>
2016-09-17 02:08:18 -0400

    rendering the whole reactdom

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 var thing = "Hello World!";
 var colors = [ "red", "blue", "orange", "green" ];
-var color = colors[Math.floor(Math.random() * colors.length)];
-
-ReactDOM.render(
-    <h1 style={ {"color":color} }>{thing}</h1>,
-    document.getElementById('example')
-);
 
 setInterval(function() {
-    color = colors[Math.floor(Math.random() * colors.length)];
-    console.log(color);
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    ReactDOM.render(
+        <h1 style={ {"color":color} }>{thing}</h1>,
+        document.getElementById('example')
+    );
 }, 1000)
