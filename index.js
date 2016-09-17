f17ddef1c1eefa2ec12efb2760e1637db4c9f82f
Jeff Fowler <jeffowler@gmail.com>
2016-09-17 02:15:32 -0400

    look it only rerenders what it needs to

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var thing = "Hello World!";
-var colors = [ "red", "blue", "orange", "green" ];
+var colors = [ "red", "blue", "orange", "green", "yellow"];
 
 setInterval(function() {
     var color = colors[Math.floor(Math.random() * colors.length)];
     ReactDOM.render(
-        <h1 style={ {"color":color} }>{thing}</h1>,
+        <div>
+            <h1 style={ {"color":"brown"} }>{thing}</h1>
+            <h1 style={ {"color":"purple"} }>{thing}</h1>
+            <h1 style={ {"color":color} }>{thing}</h1>
+            <h1 style={ {"color":"pink"} }>{thing}</h1>
+            <h1 style={ {"color":"indigo"} }>{thing}</h1>
+        </div>,
         document.getElementById('example')
     );
-}, 1000)
+}, 100)
