f0c7a29e8e1bad014ec785070b64bbb7b1d5b113
Jeff Fowler <jeffowler@gmail.com>
2016-09-17 00:54:20 -0400

    random color on page load

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var thing = "Hello World!";
+var colors = [ "red", "blue", "orange", "green" ];
+var color = colors[Math.floor(Math.random() * colors.length)];
 
 ReactDOM.render(
-    <h1 style={ {"color":"red"} }>{thing}</h1>,
+    <h1 style={ {"color":color} }>{thing}</h1>,
     document.getElementById('example')
 );
