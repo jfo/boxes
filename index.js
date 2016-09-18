459c4bad8da5a24a36a6b092142569c859deb534
Jeff Fowler <jeffowler@gmail.com>
2016-09-18 16:16:55 -0400

    components examples

--- index.js
+++ index.js
@@ -3,14 +3,32 @@ var randColor = function() {
     return '#'+Math.floor(Math.random()*16777215).toString(16);
 }
 
+
+var ExampleOne = (props) => <h1 style={{color: props.color}}>{props.text}</h1>;
+
+var ExampleTwo = function(props) {
+    return <h1 style={{color: props.color}}>{props.text}</h1>;
+}
+
+var ExampleThree = React.createClass({
+    render() {
+        return <h1 style={{color:this.props.color}}>{this.props.text}</h1>;
+    }
+})
+
+class ExampleFour extends React.Component {
+    render() {
+        return <h1 style={{color:this.props.color}}>{this.props.text}</h1>;
+    }
+}
+
 setInterval(function() {
     ReactDOM.render(
         <div>
-            <h1 style={ {"color":"brown"} }>{thing}</h1>
-            <h1 style={ {"color":"purple"} }>{thing}</h1>
-            <h1 style={ {"color":randColor()} }>{thing}</h1>
-            <h1 style={ {"color":"pink"} }>{thing}</h1>
-            <h1 style={ {"color":"indigo"} }>{thing}</h1>
+            <ExampleOne text="it came from a stabby proc function!" color={randColor()} />
+            <ExampleTwo text="it came from a function!" color={randColor()} />
+            <ExampleThree text="it came from React.createClass!" color={randColor()} />
+            <ExampleFour text="it came from an ES6 class that extends React.Component" color={randColor()} />
         </div>,
         document.getElementById('example')
     );
